78c2bb33f274bf29193aafe547d4a61c7b84f0f1
Thiago <thiago.tanaka.pec@gmail.com>
2025-03-30 14:55:49 -0300

    ajustes para o deploy

--- Back-end/api/server.js
+++ Back-end/api/server.js
@@ -1,22 +1,31 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Ola mundo!");
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../Front-End/dist")));
+
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../Front-End/dist/index.html"));
 });
 
 app.listen(port, () => {
